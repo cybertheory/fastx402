b9f941d67461acd437a11dbab1ff66f4c3c5a7a5
ArtNovaIT <rishabhsingh@berkeley.edu>
2025-12-12 10:56:40 -0500

    build fixes

--- site/next.config.ts
+++ site/next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // Configure Turbopack to resolve path aliases
-  turbopack: {
-    resolveAlias: {
-      "@": path.resolve(__dirname),
-    },
-  },
 };
 
 export default nextConfig;
