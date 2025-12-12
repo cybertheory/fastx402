2e188f8e70508afa8403f042dee886289dd378cc
ArtNovaIT <rishabhsingh@berkeley.edu>
2025-12-12 10:26:54 -0500

    build fixes

--- site/next.config.ts
+++ site/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Silence Turbopack warning
+  turbopack: {},
 };
 
 export default nextConfig;
